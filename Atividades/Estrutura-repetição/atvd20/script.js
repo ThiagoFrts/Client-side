e7cc2ee5c9dac66e32ec14f0570d855ada6e22d4
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-02 10:11:19 -0300

    Add files via upload

---
+++ Atividades/Estrutura-repetição/atvd20/script.js
@@ -0,0 +1,10 @@
+// 20. Contar quantos números entre 1 e 100 são divisíveis por 7 com for
+// Crie um programa que percorra os números de 1 a 100 e conte quantos são divisíveis por 7.
+// Exiba o total no final.
+
+for(let i = 0; i <= 100; i++){
+   if(i % 7 == 0){
+      alert(`${i}`)
+   }
+
+}
