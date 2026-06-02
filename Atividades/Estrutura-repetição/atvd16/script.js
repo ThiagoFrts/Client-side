cbac73f78d7af2832bab828538bbd64d9272046c
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-02 10:09:13 -0300

    Add files via upload

---
+++ Atividades/Estrutura-repetição/atvd16/script.js
@@ -0,0 +1,9 @@
+// 16. Mostrar múltiplos de 3 com for
+// Escreva um programa que mostre todos os múltiplos de 3 entre 1 e 30, usando a estrutura for.
+
+for (let i = 1; i <= 30; i++) {
+
+    if (i % 3 == 0) {
+        alert(`Os números múltiplos de 3 são: ${i}`)
+    }
+}
