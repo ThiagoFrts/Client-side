2a512507279c071b71377c2f6728fa28bd932371
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-02 10:10:27 -0300

    Add files via upload

---
+++ Atividades/Estrutura-repetição/atvd18/script.js
@@ -0,0 +1,12 @@
+// 18. Mostrar os primeiros 10 números ímpares com for
+// Crie um programa que exiba os 10 primeiros números ímpares positivos usando a estrutura
+// for.
+
+for (let i = 0; i <= 10; i++) {
+   if (i % 2 == 0) {
+      
+   }
+   else{
+      alert(`${i}`)
+   }
+}
