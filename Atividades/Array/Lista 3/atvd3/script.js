bbce53d87d0dd51f097c95efabf0f09fc239717c
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-09 10:54:38 -0300

    Update script.js

--- Atividades/Array/Lista 3/atvd3/script.js
+++ Atividades/Array/Lista 3/atvd3/script.js
@@ -1,12 +1,16 @@
+
+// 3. Mostrar apenas os números positivos
+// Ler 5 números inteiros e exibir apenas os números positivos armazenados no vetor.
+
 let num = [];
-let soma = 0
 
 for (let i = 0; i < 5; i++) {
     num[i] = Number(window.prompt(`Digite seus números: `))
-    soma = soma + num[i]
-    
+
 }
 
 for (let i = 0; i < 5; i++) {
-    alert(`A soma é ${soma}`)
+    if (num[i] > 0) {
+        alert(`Os números positivos são: ${num[i]}`)
+    }
 }
