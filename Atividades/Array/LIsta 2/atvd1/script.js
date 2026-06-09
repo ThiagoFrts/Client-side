a7acfb049c71cb7d9a356a0a5dce5861bc877ac8
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-09 09:24:17 -0300

    Update script.js

--- Atividades/Array/LIsta 2/atvd1/script.js
+++ Atividades/Array/LIsta 2/atvd1/script.js
@@ -1,6 +1,11 @@
+// 1. Inverter os valores
+// Ler 5 números inteiros e mostrar os valores na ordem inversa da que foram digitados.
+
 let num = [];
 for (let i = 0; i < 5; i++) {
     num[i] = Number(window.prompt(`Digite seus números: `))
 } 
 
-alert(num)
+for (let i = 4; i >= 0; i--) {
+    alert(num[i])
+}
