26eb9f2d13930f2d946f508857c2f0e105dd6182
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-09 10:54:08 -0300

    Update script.js

--- Atividades/Array/Lista 3/atvd2/script.js
+++ Atividades/Array/Lista 3/atvd2/script.js
@@ -1,10 +1,20 @@
+// 2. Contar números ímpares
+// Ler 5 números inteiros e informar quantos deles são ímpares.
+
+
 let num = [];
+let impar = 0;
 for (let i = 0; i < 5; i++) {
     num[i] = Number(window.prompt(`Digite seus números: `))
 }
 
 for (let i = 0; i < 5; i++) {
-    if (num[i] > 10) {
-        alert(num[i])
+    if (num[i] % 2 != 0) {
+        impar++
+        
     }
 }
+
+alert(`Os números ímpares são: ${impar}`)
+
+
