c396d0d03ef10905cde0d7507120283e7acc078f
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-09 09:24:56 -0300

    Update script.js

--- Atividades/Array/LIsta 2/atvd2/script.js
+++ Atividades/Array/LIsta 2/atvd2/script.js
@@ -1,10 +1,25 @@
+// 2. Verificar existência de número
+// Ler 5 números inteiros e perguntar ao usuário um número. Verificar se esse número
+// está no vetor e mostrar uma mensagem informando se foi encontrado ou não.
+
 let num = [];
+let n1;
+let encontrou = false;
 for (let i = 0; i < 5; i++) {
     num[i] = Number(window.prompt(`Digite seus números: `))
 }
 
+n1 = Number(window.prompt(`Agora digite um número: `))
+
+
 for (let i = 0; i < 5; i++) {
-    if (num[i] > 10) {
-        alert(num[i])
+    if (num[i] == n1) {
+        encontrou = true;
     }
+
+}
+if (encontrou) {
+    alert(`Número encontrado.`);
+} else {
+    alert(`Número não encontrado.`);
 }
