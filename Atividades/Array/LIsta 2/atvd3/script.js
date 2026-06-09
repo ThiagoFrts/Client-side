e7fa6857397734ebbc357590b45bb41e55584486
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-09 09:25:16 -0300

    Update script.js

--- Atividades/Array/LIsta 2/atvd3/script.js
+++ Atividades/Array/LIsta 2/atvd3/script.js
@@ -1,12 +1,28 @@
-let num = [];
-let soma = 0
+// 3. Contar quantas vezes um número aparece
+// Ler 5 números inteiros e perguntar ao usuário um número. Mostrar quantas vezes esse
+// número aparece no vetor.
 
+
+let num = [];
+let n1;
+let encontrou = false;
+let acho = 0;
 for (let i = 0; i < 5; i++) {
     num[i] = Number(window.prompt(`Digite seus números: `))
-    soma = soma + num[i]
-    
 }
 
+n1 = Number(window.prompt(`Agora digite um número: `))
+
+
 for (let i = 0; i < 5; i++) {
-    alert(`A soma é ${soma}`)
+    if (num[i] == n1) {
+        encontrou = true;
+        acho++
+    }
+
+}
+if (encontrou) {
+    alert(`Número encontrado. x${acho}`);
+} else {
+    alert(`Número não encontrado.`);
 }
