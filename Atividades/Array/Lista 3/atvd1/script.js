6fbae5becfb79423474cc047356b50ca5c2be2a8
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-09 10:53:42 -0300

    Update script.js

--- Atividades/Array/Lista 3/atvd1/script.js
+++ Atividades/Array/Lista 3/atvd1/script.js
@@ -1,6 +1,14 @@
+// 1. Encontrar o menor número
+// Ler 5 números inteiros e mostrar qual foi o menor número digitado.
+
 let num = [];
+let menor = Infinity;
 for (let i = 0; i < 5; i++) {
     num[i] = Number(window.prompt(`Digite seus números: `))
-} 
+    if (num[i] < menor) {
+        menor = num[i]
+    }
+}
+alert(`O menor número digitado é: ${menor}`)
 
-alert(num)
+
