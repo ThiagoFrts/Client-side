f31e8724c737188efd46ccfebc7b51d75e7e5230
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-15 09:24:34 -0300

    Update script.js

--- Atividades/Eventos Click/Lista 2/atvd2/script.js
+++ Atividades/Eventos Click/Lista 2/atvd2/script.js
@@ -1,2 +1,33 @@
-function medroso(){
-}alert ("Preguiçoso")
+function somar() {
+    let n1 = Number(window.prompt(`Digite um número: `))
+    let n2 = Number(window.prompt(`Digite outro número: `))
+    let resultado = n1 + n2
+    alert(`A soma dos números é: ${resultado}`)
+
+}
+
+
+function multiplicar() {
+    let n1 = Number(window.prompt(`Digite um número: `))
+    let n2 = Number(window.prompt(`Digite outro número: `))
+    let resultado = n1 * n2
+    alert(`A multiplicação dos números é: ${resultado}`)
+
+}
+
+
+function comparar() {
+    let n1 = Number(window.prompt(`Digite um número: `))
+    let n2 = Number(window.prompt(`Digite outro número: `))
+    
+    if(n1 > n2){
+        alert("O primeiro número é maior que o segundo.")
+    }
+    else if(n2 > n1){
+        alert("O segundo número é maior.")
+    }
+    else{
+        alert("Os números são iguais.")
+    }
+
+}
