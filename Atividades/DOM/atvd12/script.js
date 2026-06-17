1e1ad246a7310337a8e87208e32caeb05aa6072d
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-17 09:57:21 -0300

    Add files via upload

---
+++ Atividades/DOM/atvd12/script.js
@@ -0,0 +1,5 @@
+// Crie uma div com formato de oval (elipse) e cor inicial cinza.
+// Use JavaScript para mudar essa cor para ciano ao carregar a página.
+
+
+document.getElementById("oval").style.backgroundColor = "cyan";
---
+++ Atividades/DOM/atvd13/script.js
@@ -0,0 +1,15 @@
+// Exercício 13
+// Desafio: Crie uma div com cor inicial cinza e um botão com o texto "Mudar para
+// Azul".
+// Ao clicar no botão, a cor da div deve mudar para azul.
+
+
+function mudar() {
+    document.getElementById("qua").style.background = "#190980"
+}
+
+function voltar() {
+    document.getElementById("qua").style.background = "#676671"
+}
+
+
