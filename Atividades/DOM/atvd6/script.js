84433aa326e1b0da8040202792bcabd58d9cc76e
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-17 09:54:48 -0300

    Add files via upload

---
+++ Atividades/DOM/atvd6/script.js
@@ -0,0 +1,6 @@
+// Desafio: Crie uma div que funcione como um banner com cor inicial branca.
+// Use JavaScript para alterar a cor dessa div para um tom de cinza escuro (#333333)
+// assim que a página abrir.
+
+document.getElementById("banner").style.background = "#605a5a"
+
---
+++ Atividades/DOM/atvd7/script.js
@@ -0,0 +1,6 @@
+// Crie uma div em formato de triângulo usando bordas no CSS e cor inicial preta.
+// Quando a página carregar, o JavaScript deve mudar a cor do triângulo para verde-
+// limão.
+
+document.getElementById("tri").style.borderBottomColor = "#34f517"
+
