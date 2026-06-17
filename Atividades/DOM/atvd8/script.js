a6a9294a92602441f57841811f02be6a1f1cc576
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-17 09:55:24 -0300

    Add files via upload

---
+++ Atividades/DOM/atvd8/script.js
@@ -0,0 +1,8 @@
+// Crie um círculo vermelho e um quadrado azul lado a lado.
+// Ao carregar a página, use JavaScript para inverter as cores entre eles:
+// • O círculo fica azul
+// • O quadrado fica vermelho
+
+document.getElementById("cri").style.borderBottomColor = "#2c099f"
+document.getElementById("qua").style.borderBottomColor = "#a91010"
+
---
+++ Atividades/DOM/atvd9/script.js
@@ -0,0 +1,11 @@
+// Crie um título <h1> com o texto na cor preta.
+// Assim que a página abrir, use JavaScript para mudar o texto para verde e o fundo
+// do título para amarelo.
+
+let h1 = document.getElementsByTagName("h1")
+h1[0].style.color = "#0c7713"
+h1[0].style.background = "#fcef39"
+ 
+
+
+ 
