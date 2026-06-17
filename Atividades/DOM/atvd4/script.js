52585acb35be7e3fbbbcd22dff663ff5059d11ae
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-17 09:53:30 -0300

    Add files via upload

---
+++ Atividades/DOM/atvd4/script.js
@@ -0,0 +1,12 @@
+// Desafio: No HTML, crie três quadrados com cor inicial cinza.
+// Usando JavaScript, mude a cor de cada quadrado para uma cor diferente:
+// • O primeiro para azul
+// • O segundo para laranja
+// • O terceiro para rosa
+// Dica: Pense em como selecionar cada quadrado individualmente para mudar sua
+// cor.
+
+let quadrado = document.getElementsByClassName("qua")
+quadrado[0].style.background = "#080887"
+quadrado[1].style.background = "#f58e07"
+quadrado[2].style.background = "#f056ba"
---
+++ Atividades/DOM/atvd5/script.js
@@ -0,0 +1,8 @@
+// Crie um parágrafo com fundo cinza claro.
+// Usando JavaScript, faça com que o fundo desse parágrafo fique preto e o texto
+// fique branco automaticamente quando a página carregar.
+
+let p = document.getElementsByTagName("p")
+p[0].style.color = "#ffffff"
+p[0].style.background = "#000000"
+ 
