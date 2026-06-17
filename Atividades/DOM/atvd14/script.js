a5fb29ae8b75ebb0426510eef9737a90cddd116d
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-17 09:57:50 -0300

    Add files via upload

---
+++ Atividades/DOM/atvd14/script.js
@@ -0,0 +1,13 @@
+// Exercício 14
+// Desafio: Crie um círculo vermelho e um botão chamado "Trocar Cor".
+// Quando o botão for clicado, o círculo deve ficar verde.
+
+function mudar() {
+    document.getElementById("cir").style.background = "#0c6d19"
+}
+
+function voltar() {
+    document.getElementById("cir").style.background = "#820b0b"
+}
+
+
---
+++ Atividades/DOM/atvd15/script.js
@@ -0,0 +1,15 @@
+// Exercício 15
+// Desafio: Crie um quadrado amarelo e dois botões:
+// • Botão 1: "Azul"
+// • Botão 2: "Rosa"
+// Quando clicar em cada botão, o quadrado deverá assumir a cor correspondente.
+
+function azul() {
+    document.getElementById("qua").style.background = "#2c10a8"
+}
+
+function rosa() {
+    document.getElementById("qua").style.background = "#a40e84"
+}
+
+
