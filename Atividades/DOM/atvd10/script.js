44bf2bbfe2ef661538e8b300d3cd83e07d67d1c6
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-17 09:56:38 -0300

    Add files via upload

---
+++ Atividades/DOM/atvd10/script.js
@@ -0,0 +1,16 @@
+// Crie cinco divs pequenas alinhadas em linha horizontal, todas com a cor inicial
+// cinza.
+// Use JavaScript para trocar cada uma para uma cor diferente do arco-íris:
+// 1. Vermelho
+// 2. Laranja
+// 3. Amarelo
+// 4. Verde
+// 5. Azul
+
+let quad = document.getElementsByClassName("qua")
+quad[0].style.background = "#750b0b"
+quad[1].style.background = "#e8781d"
+quad[2].style.background = "#e1de1f"
+quad[3].style.background = "#0c5f05"
+quad[4].style.background = "#0f0b75"
+ 
---
+++ Atividades/DOM/atvd11/script.js
@@ -0,0 +1,8 @@
+// Crie uma div com cor de fundo rosa e um parágrafo dentro dela com texto preto.
+// Quando a página carregar, o JavaScript deve mudar:
+// • Div → preta
+// • Texto do parágrafo → rosa
+
+document.getElementsByClassName("pai")[0].style.background = "#000000"
+document.getElementsByTagName("p")[0].style.color = "#ab149c"
+
