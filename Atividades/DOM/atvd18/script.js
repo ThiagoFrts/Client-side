e3b96f12297a11b774dec0fd27e03d26451ba804
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-18 09:11:46 -0300

    Add files via upload

---
+++ Atividades/DOM/atvd18/script.js
@@ -0,0 +1,16 @@
+// Exercício 18
+// Desafio: Crie três quadrados cinza e um botão chamado "Colorir".
+// Ao clicar no botão:
+// • O primeiro quadrado deve ficar vermelho.
+// • O segundo deve ficar verde.
+// • O terceiro deve ficar azul.
+
+function colorir() {
+    document.getElementsByClassName("qua")[0].style.background = "#850d0d"
+    document.getElementsByClassName("qua")[1].style.background = "#186f10"
+    document.getElementsByClassName("qua")[2].style.background = "#090d68"
+}
+
+
+
+
---
+++ Atividades/DOM/atvd19/script.js
@@ -0,0 +1,13 @@
+// Desafio: Crie um título <h1> com texto preto e um botão chamado "Destacar".
+// Quando o botão for clicado:
+// • O texto deve ficar branco.
+// • O fundo do título deve ficar azul.
+
+function destacar() {
+    document.getElementsByTagName("h1")[0].style.background = "#080a7c"
+    document.getElementsByTagName("h1")[0].style.color = "#eaeaf6"
+}
+
+
+
+
