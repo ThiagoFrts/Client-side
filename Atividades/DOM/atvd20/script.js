b9f08ef761097f3b330d918edfd6b4f62a653604
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-18 09:12:25 -0300

    Add files via upload

---
+++ Atividades/DOM/atvd20/script.js
@@ -0,0 +1,12 @@
+// Exercício 20
+// Desafio: Crie um círculo azul e um botão chamado "Esconder".
+// Ao clicar no botão, o círculo deve desaparecer da tela.
+// Dica: Pesquise sobre a propriedade display.F
+
+function esconder() {
+    document.getElementById("cir").style.display = "none"
+}
+
+
+
+
---
+++ Atividades/DOM/atvd21/script.js
@@ -0,0 +1,9 @@
+// Exercício 21
+// Desafio: Crie uma div verde e um botão chamado "Mostrar Mensagem".
+// Ao clicar no botão, altere o texto da div para:
+// "Cor alterada com sucesso!"
+
+function mostrarMensagem() {
+    document.getElementById("caixa").innerHTML = "Cor alterada com sucesso!"
+}
+
