932a2607c1ffadbaeb0af7178a6a53f8075e7bb0
Thiago Freitas <thiagofreitasreis2018@gmail.com>
2026-06-18 09:13:06 -0300

    Add files via upload

---
+++ Atividades/DOM/atvd22/script.js
@@ -0,0 +1,16 @@
+// Crie cinco divs pequenas alinhadas em linha horizontal, todas com a cor inicial
+// cinza.
+// Use JavaScript para trocar cada uma para uma cor diferente do arco-íris:
+// 1. Vermelho
+// 2. Laranja
+// 3. Amarelo
+// 4. Verde
+// 5. Azul
+
+function colorir() {
+    document.getElementsByClassName("qua")[0].style.background = "#850d0d"
+    document.getElementsByClassName("qua")[1].style.background = "#e89817"
+    document.getElementsByClassName("qua")[2].style.background = "#ffea01"
+    document.getElementsByClassName("qua")[3].style.background = "#198020"
+    document.getElementsByClassName("qua")[4].style.background = "#171c84"
+}
